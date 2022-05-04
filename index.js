70383dfab8df08f9df2bde5315f5afbe79c0ccd2
Priyanshub9931 <test@w3schools.com>
2022-05-04 12:49:44 +0530

    Add two number

--- index.js
+++ index.js
@@ -4,7 +4,4 @@
 
 /* I love to teach and play with kids */
 
-const FCC = "freeCodeCamp"; // Change this line
-let fact = "is cool!"; // Change this line
-fact = "is awesome!";
-console.log(FCC, fact); // Change this line
+const sum = 10 + 10;
