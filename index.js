2d3662c95c8a1db47baa565a55d8758b1f1cf517
Priyanshub9931 <test@w3schools.com>
2022-05-04 18:43:30 +0530

    Global Scope and Functions

--- index.js
+++ index.js
@@ -4,7 +4,23 @@
 
 /* I love to teach and play with kids */
 
-function timesFive(num){
-    return num * 5;
+// Declare the myGlobal variable below this line
+let myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+oopsGlobal = 5;
+}
+
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
   }
-   var answer = timesFive(5);
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
