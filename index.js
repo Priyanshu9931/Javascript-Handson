3bea8868a001f69e3be679a9e6f8099ee9e8de6c
Priyanshub9931 <test@w3schools.com>
2022-05-04 14:52:28 +0530

    Concatenating Strings with the Plus Equals Operator

--- index.js
+++ index.js
@@ -4,4 +4,5 @@
 
 /* I love to teach and play with kids */
 
-const myStr = "This is the start." + " This is the end.";
+let myStr = "This is the first sentence.";
+myStr += " This is the second sentence.";
