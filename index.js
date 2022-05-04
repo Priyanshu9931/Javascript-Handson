83cf26ea591766e795f6e2ef3f42ece721a2f49f
Priyanshub9931 <test@w3schools.com>
2022-05-04 17:04:28 +0530

    Use Bracket Notation to Find the Nth Character in a String

--- index.js
+++ index.js
@@ -5,8 +5,7 @@
 /* I love to teach and play with kids */
 
 // Setup
-let myStr = "Jello World";
+const lastName = "Lovelace";
 
 // Only change code below this line
-myStr = "Hello World"; // Change this line
-// Only change code above this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
