7643e9ca27c9ee2c295e8f4e4d62f764e84f514a
Priyanshub9931 <test@w3schools.com>
2022-05-04 18:17:45 +0530

    shopping list

--- index.js
+++ index.js
@@ -4,10 +4,4 @@
 
 /* I love to teach and play with kids */
 
-// Setup
-// Setup
-const myArray = [["John", 23], ["dog", 3]];
-myArray.shift();
-
-// Only change code below this line
-myArray.unshift(["Paul", 35]);
+const myList = [["Chocolate Bar", 15],["Milk",10],["DairyMilk",50],["Curd",42],["Mady",45]];
