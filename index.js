982bcf01e5a29ef4ed50c5f946f9b758d3a89972
Priyanshub9931 <test@w3schools.com>
2022-05-04 13:36:40 +0530

    Quoting Strings with Single Quotes

--- index.js
+++ index.js
@@ -4,4 +4,4 @@
 
 /* I love to teach and play with kids */
 
-const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
