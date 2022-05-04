1b4e38fae08248e4de7cd3d818dd80221ae98202
Priyanshub9931 <test@w3schools.com>
2022-05-04 18:51:14 +0530

    Local Scope and Functions

--- index.js
+++ index.js
@@ -4,23 +4,14 @@
 
 /* I love to teach and play with kids */
 
-// Declare the myGlobal variable below this line
-let myGlobal = 10;
 
-function fun1() {
-  // Assign 5 to oopsGlobal Here
-oopsGlobal = 5;
-}
-
-// Only change code above this line
-
-function fun2() {
-  var output = "";
-  if (typeof myGlobal != "undefined") {
-    output += "myGlobal: " + myGlobal;
+function myLocalScope() {
+    // Only change code below this line
+   var myVar ="hii";
+    console.log('inside myLocalScope', myVar);
   }
-  if (typeof oopsGlobal != "undefined") {
-    output += " oopsGlobal: " + oopsGlobal;
-  }
-  console.log(output);
-}
+  myLocalScope();
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar);
