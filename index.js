74b03fb9dcbc003de98d7bc47917c2874ee8e3a3
Priyanshub9931 <test@w3schools.com>
2022-05-05 16:45:21 +0530

    Use Multiple Conditional (Ternary) Operators

--- index.js
+++ index.js
@@ -6,8 +6,8 @@
 
 
 
-function checkEqual(a, b) {
-  return a === b ? "Equal" : "Not Equal";
- }
- 
- checkEqual(1, 2);
+function checkSign(num) {
+  return num > 0 ? "positive"
+    : num < 0 ? "negative"
+    : "zero";
+}
