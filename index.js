84810273bf661627be3220cf5829194c2d47663c
Priyanshub9931 <test@w3schools.com>
2022-05-05 13:30:21 +0530

    Accessing Nested Objects

--- index.js
+++ index.js
@@ -5,27 +5,16 @@
 /* I love to teach and play with kids */
 
 
-const myMusic = [
-  {
-    "artist": "Billy Joel",
-    "title": "Piano Man",
-    "release_year": 1973,
-    "formats": [
-      "CD",
-      "8T",
-      "LP"
-    ],
-    "gold": true
-  },
-
-  {
-      "artist": "A.R Raheman",
-    "title": "Piano Man",
-    "release_year": 2000,
-    "formats": [
-      "CD",
-      "8T",
-      "LP"
-    ],
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
   }
-];
+};
+
+const gloveBoxContents = undefined;
