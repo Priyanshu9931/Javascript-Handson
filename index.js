c678fb9d46a94ee081b0ca54f0ecbf5cbe1debcb
Priyanshub9931 <test@w3schools.com>
2022-05-05 14:55:18 +0530

    Record Collection

--- index.js
+++ index.js
@@ -5,23 +5,14 @@
 /* I love to teach and play with kids */
 
 
-const myPlants = [
-  {
-    type: "flowers",
-    list: [
-      "rose",
-      "tulip",
-      "dandelion"
-    ]
-  },
-  {
-    type: "trees",
-    list: [
-      "fir",
-      "pine",
-      "birch"
-    ]
+function updateRecords(records, id, prop, value) {
+  if (value === '') {
+    delete records[id][prop];
+  } else if (prop === 'tracks') {
+    records[id][prop] = records[id][prop] || []; // this is called shortcircuit evaluation, see below for explanation
+    records[id][prop].push(value);
+  } else {
+    records[id][prop] = value;
   }
-];
-
-const secondTree = myPlants[1].list[1];
+  return records;
+}
