11531b1d18bfd4bb05f645bb4d7d134d90ea52e0
Priyanshub9931 <test@w3schools.com>
2022-05-05 12:40:07 +0530

    Add New Properties to a JavaScript Object

--- index.js
+++ index.js
@@ -4,14 +4,11 @@
 
 /* I love to teach and play with kids */
 
-// Setup
 const myDog = {
-  "name": "Coder",
+  "name": "Happy Coder",
   "legs": 4,
   "tails": 1,
   "friends": ["freeCodeCamp Campers"]
 };
 
-// Only change code below this line
-
-myDog.name = "Happy Coder";
+myDog.bark = "bark";
