e0d9fcdc28aefd6aba5a92a44112cc4b0d5de60b
Priyanshub9931 <test@w3schools.com>
2022-05-05 10:14:48 +0530

    Stand in Line

--- index.js
+++ index.js
@@ -6,11 +6,21 @@
 
 
 // Setup
-let processed = 2;
+function nextInLine(arr, item) {
+  // Only change code below this line
+   var queue = arr.push(item);
 
-function processArg(num) {
-  return (num + 3) / 5;
+  var removeItem = arr.shift();
+
+  return removeItem;
+  return item;
+  // Only change code above this line
 }
 
-// Only change code below this line
-processed = processArg(7);
+// Setup
+const testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
