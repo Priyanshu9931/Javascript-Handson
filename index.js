88ff39268b396ddb02287af8146905625852417d
Priyanshub9931 <test@w3schools.com>
2022-05-05 15:08:14 +0530

    Iterate with JavaScript While Loops

--- index.js
+++ index.js
@@ -4,15 +4,14 @@
 
 /* I love to teach and play with kids */
 
+// Setup
+const myArray = [];
 
-function updateRecords(records, id, prop, value) {
-  if (value === '') {
-    delete records[id][prop];
-  } else if (prop === 'tracks') {
-    records[id][prop] = records[id][prop] || []; // this is called shortcircuit evaluation, see below for explanation
-    records[id][prop].push(value);
-  } else {
-    records[id][prop] = value;
-  }
-  return records;
+// Only change code below this line
+
+let i = 5;
+
+while (i >= 0) {
+  myArray.push(i);
+  i--;
 }
