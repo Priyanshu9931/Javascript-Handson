2c5a026b71aca5070b899861904bf5acdaadffea
Priyanshub9931 <test@w3schools.com>
2022-05-05 16:05:32 +0530

    Generate Random Fractions with JavaScript

--- index.js
+++ index.js
@@ -4,11 +4,20 @@
 
 /* I love to teach and play with kids */
 
-function sum(arr, n) {
 
-  if(n <= 0) {
-    return 0;
-  } else {
-    return sum(arr, n - 1) + arr[n - 1];
-  }
+
+function randomFraction() {
+
+  // Only change code below this line
+var x , a , b;
+x= 0;
+a = 0;
+b = 0;
+x += Math.random();
+a += Math.random();
+b = x/a;
+return b;
+  
+
+  // Only change code above this line
 }
