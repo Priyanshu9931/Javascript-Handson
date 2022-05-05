8051f9ca23661de43e33b275da71ae765cf66268
Priyanshub9931 <test@w3schools.com>
2022-05-05 12:20:19 +0530

    Build JavaScript Objects

--- index.js
+++ index.js
@@ -4,35 +4,12 @@
 
 /* I love to teach and play with kids */
 
-
-let count = 0;
-
-function cc(card) {
+const myDog = {
   // Only change code below this line
-switch (card) {
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-      count++;
-      break;
-    case 10:
-    case "J":
-    case "Q":
-    case "K":
-    case "A":
-      count--;
-      break;
-  }
-  if (count > 0) {
-    return count + " Bet";
-  } else {
-    return count + " Hold";
-  }
+"name": "Bull Dog",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Milk", "Eggs"]
 
-  return "Change Me";
   // Only change code above this line
-}
-
-cc(2); cc(3); cc(7); cc('K'); cc('A');
+};
