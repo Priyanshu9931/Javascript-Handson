006ab9a6febb6750b02e66ab19b75bb2a0d254c6
Priyanshub9931 <test@w3schools.com>
2022-05-05 10:18:43 +0530

    Understanding Boolean Values

--- index.js
+++ index.js
@@ -5,22 +5,10 @@
 /* I love to teach and play with kids */
 
 
-// Setup
-function nextInLine(arr, item) {
+function welcomeToBooleans() {
   // Only change code below this line
-   var queue = arr.push(item);
 
-  var removeItem = arr.shift();
+  return true; // Change this line
 
-  return removeItem;
-  return item;
   // Only change code above this line
 }
-
-// Setup
-const testArr = [1, 2, 3, 4, 5];
-
-// Display code
-console.log("Before: " + JSON.stringify(testArr));
-console.log(nextInLine(testArr, 6));
-console.log("After: " + JSON.stringify(testArr));
